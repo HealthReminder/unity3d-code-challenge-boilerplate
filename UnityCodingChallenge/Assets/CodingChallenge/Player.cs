9bacd61da27a32db508dc3ee7b554849279d83fe
Eduardo-PC <devugates@dronekids.com.br>
2021-08-19 17:19:39 -0700

    Early Player
    Implemented a Player script that contains an abstract class for properties, like walking speed. Added an abstract class for data, like current health or velocity. Created an abstract class for the playerController, responsible for the functions of movement, for example. And implemented the Player script that receives input from the player directly and store interaction information, like rays from the camera (This script is equivalent to a PlayerInput script mostly). Created a tag for interactable objects.

---
+++ UnityCodingChallenge/Assets/CodingChallenge/Player.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public abstract class APlayerProperties : MonoBehaviour
+{
+    internal float walkSpeed = 0.1f;
+    internal float minDistanceToTarget = 0.25f;
+}
+public abstract class APlayerData : APlayerProperties
+{
+    public int Health;
+    public int Coins;
+}
+public abstract class APlayerController: APlayerData
+{
+    public bool MoveTowardsPoint(Vector3 point, float minDistance)
+    {
+        if (Vector3.Distance(transform.position, point) <= minDistance)
+            return true;
+        else
+        {
+            transform.position += (point - transform.position).normalized * walkSpeed;
+            return false;
+        }        
+    }
+
+    public void GetInformationFromRay(Camera cam,out bool isHit, out Vector3 hitPos, out GameObject hitInteractable)
+    {
+        isHit = false;
+        hitPos = Vector3.zero;
+        hitInteractable = null;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            isHit = true;
+            hitPos = hit.point;
+            if (hit.transform.tag == "Interactable")
+                hitInteractable = hit.transform.gameObject;
+        }
+    }
+}
+public class Player : APlayerController
+{
+    public Camera PlayerCamera;
+    public Vector3 currentTarget;
+    public bool IsInteracting = false;
+    public bool IsOnTarget = false;
+    private void Awake()
+    {
+        currentTarget = transform.position;
+    }
+    private void Update()
+    {
+        if (!IsInteracting)
+            if(!IsOnTarget)
+                IsOnTarget = MoveTowardsPoint(currentTarget, minDistanceToTarget);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Player is trying to interact with the world
+            GetInformationFromRay(PlayerCamera, out bool isValid, out Vector3 mousePos, out GameObject interactingWith);
+            if (isValid)
+            {
+                bool interactionResult = false;
+                if (interactingWith != null)
+                {
+                    interactionResult = false;
+                    Debug.Log($"Player interacted with building with a {interactionResult} result.");
+                }
+                if (!interactionResult)
+                {
+                    //Player could not interact with object
+                    //That means that it has a new target to go to
+                    currentTarget = mousePos;
+                    IsOnTarget = false;
+
+                }
+            }
+
+        }
+    }
+ 
+}
